8882d8fc737a91b2ed2db5bc4b81aab69e295c14
Visual Student <visualstudent@zschie.edu.pl>
2023-02-13 08:24:05 +0100

    zadanie skonczone

--- Przelicznikwalut/MainWindow.xaml.cs
+++ Przelicznikwalut/MainWindow.xaml.cs
@@ -20,6 +20,9 @@ namespace Przelicznikwalut
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const double zlotyToGBP = 0.19;
+        private const double zlotytoUSD = 0.22;
+        private const double USDtoGBP = 0.83;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +30,43 @@ namespace Przelicznikwalut
 
         private void Konwertuj_Click(object sender, RoutedEventArgs e)
         {
+            double ilosc = double.Parse(IloscTextBox.Text);
+            string zw = ZComboBox.Text;
+            string dow = DoComboBox.Text;
 
+            double Przeliczona = Math.Round(Konwertuj(ilosc, zw, dow),2);
+            Przeliczonakwota.Text =Przeliczona.ToString();
+        }
+        private double Konwertuj(double ilosc,string zw,string dow)
+        {
+            if(ZComboBox.Text == "Złoty" && DoComboBox.Text == "Funt")
+            {
+                return ilosc*zlotyToGBP;
+            }
+            if (ZComboBox.Text == "Złoty" && DoComboBox.Text == "Dolar")
+            {
+                return ilosc * zlotytoUSD;
+            }
+            if (ZComboBox.Text == "Funt" && DoComboBox.Text == "Dolar")
+            {
+                return ilosc / USDtoGBP;
+            }
+            if (ZComboBox.Text == "Funt" && DoComboBox.Text == "Złoty")
+            {
+                return ilosc / zlotyToGBP;
+            }
+            if (ZComboBox.Text == "Dolar" && DoComboBox.Text == "Funt")
+            {
+                return ilosc * USDtoGBP;
+            }
+            if (ZComboBox.Text == "Dolar" && DoComboBox.Text == "Złoty")
+            {
+                return ilosc / zlotytoUSD;
+            }
+            else
+            {
+                return ilosc;
+            }
         }
     }
 }
